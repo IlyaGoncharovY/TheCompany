c5b05e687d4fe87dce4dc1b8609e318a0a5434c5
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-12-29 04:11:30 +0300

    feat(company reducer) added CompanyReducer.ts

---
+++ TheCompany/src/features/company/reducer/CompanyReducer.ts
@@ -0,0 +1,20 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+interface initialStateType {
+
+}
+
+const initialState: initialStateType = {
+
+};
+
+const CompanyReducer = createSlice({
+  name: 'CompanyReducer',
+  initialState,
+  reducers: {
+
+  },
+});
+export const {} = CompanyReducer.actions;
+
+export default CompanyReducer.reducer;
--- TheCompany/src/store/store.ts
+++ TheCompany/src/store/store.ts
@@ -1,8 +1,10 @@
 import {Action, configureStore, ThunkAction} from '@reduxjs/toolkit';
 
+import {CompanyReducer} from '../features/company/index.ts';
+
 export const store = configureStore({
   reducer: {
-   
+    company: CompanyReducer,
   },
 });
 
