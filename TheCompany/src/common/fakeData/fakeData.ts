720d247f8d9b716a85e02bb98122b2abffeb442f
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-12-30 01:06:05 +0300

    fix(fakeData) simple code

--- TheCompany/src/common/fakeData/fakeData.ts
+++ TheCompany/src/common/fakeData/fakeData.ts
@@ -1,53 +1,54 @@
 import {v1} from 'uuid';
 
-export type CurrWorkerType = {
-  id: string;
-  secondName: string;
-  name: string;
-  post: string;
-  isEdit: boolean;
-}
-
-export type WorkerType = {
-  [companyId: string]: CurrWorkerType[]
-};
-
-export type CompanyType = {
-  id: string;
-  name: string;
-  count: number;
-  address: string;
-  isEdit: boolean
-};
-
-const generateWorker = (): { id: string; secondName: string; name: string; post: string; isEdit: boolean } => ({
-  id: v1(),
-  secondName: 'FakeSecondName1',
-  name: 'FakeName',
-  post: 'FakePost',
-  isEdit: false,
-});
-
-const generateCompany = (id: string, name: string, address: string, numWorkers: number, isEdit: boolean): CompanyType => {
-  const workers = Array.from({length: numWorkers}, () => generateWorker());
-  return {
-    id,
-    name,
-    count: workers.length,
-    address,
-    isEdit,
-  };
-};
-
 const companyId1 = v1();
 const companyId2 = v1();
+const companyId3 = v1();
 
-export const workers: WorkerType = {
-  [companyId1]: [generateWorker(), generateWorker(), generateWorker()],
-  [companyId2]: [generateWorker(), generateWorker()],
+export type CurrWorkersType = {
+  id: string
+  companyId: string
+  isChecked: boolean
+  secondName: string
+  name: string
+  post: string
+}
+
+export type InitialWorkersType = {[key: string]: CurrWorkersType[]}
+
+export const workers: InitialWorkersType = {
+  [companyId1] : [
+    {id: v1(), companyId: companyId1, isChecked: false, secondName:'secondName1.1', name: 'name1.1', post:'post1.1'},
+    {id: v1(), companyId: companyId1, isChecked: false, secondName:'secondName1.2', name: 'name1.2', post:'post1.2'},
+    {id: v1(), companyId: companyId1, isChecked: false, secondName:'secondName1.3', name: 'name1.3', post:'post1.3'},
+  ],
+  [companyId2] : [
+    {id: v1(), companyId: companyId2, isChecked: false, secondName:'secondName2.1', name: 'name2.1', post:'post2.1'},
+    {id: v1(), companyId: companyId2, isChecked: false, secondName:'secondName2.2', name: 'name2.2', post:'post2.2'},
+    {id: v1(), companyId: companyId2, isChecked: false, secondName:'secondName2.3', name: 'name2.3', post:'post2.3'},
+    {id: v1(), companyId: companyId2, isChecked: false, secondName:'secondName2.4', name: 'name2.4', post:'post2.4'},
+  ],
+  [companyId3]: [
+    {id: v1(), companyId: companyId3, isChecked: false, secondName:'secondName3.1', name: 'name3.1', post:'post3.1'},
+    {id: v1(), companyId: companyId3, isChecked: false, secondName:'secondName3.2', name: 'name3.2', post:'post3.2'},
+    {id: v1(), companyId: companyId3, isChecked: false, secondName:'secondName3.3', name: 'name3.3', post:'post3.3'},
+    {id: v1(), companyId: companyId3, isChecked: false, secondName:'secondName3.4', name: 'name3.4', post:'post3.4'},
+    {id: v1(), companyId: companyId3, isChecked: false, secondName:'secondName3.5', name: 'name3.5', post:'post3.5'},
+  ],
 };
 
-export const companies: CompanyType[] = [
-  generateCompany(companyId1, 'company1', 'company1 address', workers[companyId1].length, false),
-  generateCompany(companyId2, 'company2', 'company2 address', workers[companyId2].length, false),
+export type InitialCompanyTypes = {
+  id: string,
+  isChecked: boolean
+  name: string
+  count: number
+  address: string
+}
+export const companies: InitialCompanyTypes[] = [
+  {id: companyId1, isChecked: false, name: 'companyId1',
+    count: workers[companyId1].length, address: 'companyId1street'},
+  {id: companyId2, isChecked: false, name: 'companyId2',
+    count: workers[companyId2].length, address: 'companyId2street'},
+  {id: companyId3, isChecked: false, name: 'companyId3',
+    count: workers[companyId3].length, address: 'companyId3street'},
 ];
+
