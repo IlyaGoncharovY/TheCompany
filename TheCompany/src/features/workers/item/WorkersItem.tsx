bc8c0e43ccf43385e2f26847db958df6ba22458f
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-12-31 01:01:56 +0300

    fix(workerItem) added function editWorkerBody added logic for edit body worker

--- TheCompany/src/features/workers/item/WorkersItem.tsx
+++ TheCompany/src/features/workers/item/WorkersItem.tsx
@@ -1,8 +1,8 @@
-import {FC} from 'react';
+import {FC, useState} from 'react';
 
 import {useAppDispatch} from '../../../store';
 import {CurrWorkersType} from '../../../common';
-import {changeWorkerStatus} from '../reducer/WorkersReducer.ts';
+import {changeWorkerStatus, editWorkerBody} from '../reducer/WorkersReducer.ts';
 
 import s from './WorkersItem.module.scss';
 
@@ -11,12 +11,43 @@ interface IWorkersItem {
 }
 export const WorkersItem: FC<IWorkersItem> = ({worker}) => {
 
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedSecondName, setEditedSecondName] = useState(worker.secondName);
+  const [editedName, setEditedName] = useState(worker.name);
+  const [editedPost, setEditedPost] = useState(worker.post);
+
   const dispatch = useAppDispatch();
 
   const onClickCheckHandler = (workerId: string) => dispatch(changeWorkerStatus(workerId));
 
+  const onDoubleClickHandler = () => {
+    setIsEditing(true);
+  };
+
+  const onSaveHandler = () => {
+    dispatch(
+      editWorkerBody({
+        companyId: worker.companyId,
+        workerId: worker.id,
+        editedSecondName,
+        editedName,
+        editedPost,
+      }),
+    );
+    setIsEditing(false);
+  };
+
+  const onKeyDownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onSaveHandler();
+    }
+  };
+  console.log(worker);
   return (
-    <tr className={`${worker.isChecked ? s.editWorker : ''}`}>
+    <tr
+      className={`${worker.isChecked ? s.editWorker : ''}`}
+      onDoubleClick={onDoubleClickHandler}
+    >
       <td>
         <input
           type="checkbox"
@@ -24,9 +55,45 @@ export const WorkersItem: FC<IWorkersItem> = ({worker}) => {
           onClick={() => onClickCheckHandler(worker.id)}
         />
       </td>
-      <td>{worker.secondName}</td>
-      <td>{worker.name}</td>
-      <td>{worker.post}</td>
+      {/*<td>{worker.secondName}</td>*/}
+      {/*<td>{worker.name}</td>*/}
+      {/*<td>{worker.post}</td>*/}
+      <td>
+        {isEditing ? (
+          <input
+            type="text"
+            value={editedSecondName}
+            onChange={(e) => setEditedSecondName(e.target.value)}
+            onKeyDown={onKeyDownHandler}
+          />
+        ) : (
+          worker.secondName
+        )}
+      </td>
+      <td>
+        {isEditing ? (
+          <input
+            type="text"
+            value={editedName}
+            onChange={(e) => setEditedName(e.target.value)}
+            onKeyDown={onKeyDownHandler}
+          />
+        ) : (
+          worker.name
+        )}
+      </td>
+      <td>
+        {isEditing ? (
+          <input
+            type="text"
+            value={editedPost}
+            onChange={(e) => setEditedPost(e.target.value)}
+            onKeyDown={onKeyDownHandler}
+          />
+        ) : (
+          worker.post
+        )}
+      </td>
     </tr>
   );
 };
