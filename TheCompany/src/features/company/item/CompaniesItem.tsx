290bfdf45350d3b0d0c758262f5e261ff81c116b
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-12-31 01:02:41 +0300

    fix(companyItem) added logic for edit company body

--- TheCompany/src/features/company/item/CompaniesItem.tsx
+++ TheCompany/src/features/company/item/CompaniesItem.tsx
@@ -1,9 +1,9 @@
-import {FC} from 'react';
+import {FC, useState} from 'react';
 
 import {InitialCompanyTypes} from '../../../common';
 
 import {useAppDispatch, useAppSelector} from '../../../store';
-import {changeStatusCompany} from '../reducer/CompanyReducer.ts';
+import {changeStatusCompany, editCompanyBody} from '../reducer/CompanyReducer.ts';
 
 import s from './CompanyItem.module.scss';
 
@@ -15,13 +15,36 @@ export const CompaniesItem: FC<ICompaniesItem> = ({company}) => {
 
   const workers = useAppSelector(state => state.workers.workers);
 
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [editedName, setEditedName] = useState<string>(company.name);
+  const [editedAddress, setEditedAddress] = useState<string>(company.address);
+
   const workersCount = workers[company.id]?.length || 0;
 
   const dispatch = useAppDispatch();
   const onClickCheckHandler = (companyId: string) => dispatch(changeStatusCompany(companyId));
 
+  const onDoubleClickHandler = () => {
+    setIsEditing(true);
+  };
+
+  const onSaveHandler = () => {
+    dispatch(editCompanyBody({ companyId: company.id, editedName, editedAddress }));
+    setIsEditing(false);
+  };
+
+  const onKeyDownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onSaveHandler();
+    }
+  };
+
+  console.log(company);
   return (
-    <tr className={`${company.isChecked ? s.editCompany : ''}`}>
+    <tr
+      className={`${company.isChecked ? s.editCompany : ''}`}
+      onDoubleClick={onDoubleClickHandler}
+    >
       <td>
         <input
           type="checkbox"
@@ -29,8 +52,32 @@ export const CompaniesItem: FC<ICompaniesItem> = ({company}) => {
           onClick={() => onClickCheckHandler(company.id)}
         />
       </td>
-      <td>{company.name}</td>
-      <td>{company.address}</td>
+      {/*<td>{company.name}</td>*/}
+      {/*<td>{company.address}</td>*/}
+      <td>
+        {isEditing ? (
+          <input
+            type="text"
+            value={editedName}
+            onChange={(e) => setEditedName(e.target.value)}
+            onKeyDown={onKeyDownHandler}
+          />
+        ) : (
+          company.name
+        )}
+      </td>
+      <td>
+        {isEditing ? (
+          <input
+            type="text"
+            value={editedAddress}
+            onChange={(e) => setEditedAddress(e.target.value)}
+            onKeyDown={onKeyDownHandler}
+          />
+        ) : (
+          company.address
+        )}
+      </td>
       <td>{workersCount}</td>
     </tr>
   );
