e9f0d261f48caa155d14d7f6c2ca4fb87cb5ecfa
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-12-31 16:29:18 +0300

    feat(CommonContainer) universal component for render Container on infinity scroll

---
+++ TheCompany/src/common/components/container/CommonContainer.tsx
@@ -0,0 +1,46 @@
+import React, {FC, useEffect, useRef, useState} from 'react';
+
+import s from './CommonContainer.module.scss';
+
+interface CommonContainerProps {
+  headerComponent: React.ReactNode;
+  renderItem: (item: any) => React.ReactNode;
+  items: any[];
+}
+
+export const CommonContainer: FC<CommonContainerProps> = ({ headerComponent, renderItem, items }) => {
+  const anchorRef = useRef<HTMLDivElement>(null);
+  const [autoScrollActive, setAutoScrollActive] = useState<boolean>(true);
+
+  const scrollHandler = (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
+    const element = e.currentTarget;
+    if (Math.abs((element.scrollHeight - element.scrollTop) - element.clientHeight) < 300) {
+      !autoScrollActive && setAutoScrollActive(true);
+    } else {
+      autoScrollActive && setAutoScrollActive(false);
+    }
+  };
+
+  useEffect(() => {
+    if (autoScrollActive) {
+      anchorRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }
+    return () => {
+      setAutoScrollActive(false);
+    };
+  }, [autoScrollActive, items]);
+
+  return (
+    <div className={s.tableContainer}>
+      <table>
+        {headerComponent}
+        <tbody onScroll={scrollHandler}>
+          {items.map((item) => (
+            <React.Fragment key={item.id}>{renderItem(item)}</React.Fragment>
+          ))}
+          <div ref={anchorRef} />
+        </tbody>
+      </table>
+    </div>
+  );
+};
