68ac7e3fe74be2e742c58b93bf4e841a5702791b
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-12-31 17:42:22 +0300

    feat(universal header) added CommonHeader.tsx

---
+++ TheCompany/src/common/components/hedeer/CommonHeader.tsx
@@ -0,0 +1,70 @@
+import {ChangeEvent, FC, useCallback} from 'react';
+
+import {tableHeaderType} from '../../dataSet.ts';
+
+import {HeaderItem} from './item/HeaderItem.tsx';
+
+interface ICommonHeader<T> {
+  items: T[];
+  checkOnChangeHandler: (e: ChangeEvent<HTMLInputElement>) => void;
+  addNewItemHandler: () => void;
+  deleteItemHandler: () => void;
+  disabledCheck?: boolean;
+  disabledAddItem?: boolean;
+  itemName: string;
+  dataForTable: tableHeaderType[]
+}
+export const CommonHeader:FC<ICommonHeader<any>> = ({
+  items,
+  checkOnChangeHandler,
+  addNewItemHandler,
+  deleteItemHandler,
+  disabledCheck,
+  disabledAddItem,
+  itemName,
+  dataForTable,
+}) => {
+
+  const checkedItemIds = items.filter((item) =>
+    item.isChecked).map((item) => item.id);
+
+  const handleAddItem = useCallback(() => {
+    addNewItemHandler();
+  }, [addNewItemHandler]);
+
+  const handleDeleteItem = useCallback(() => {
+    deleteItemHandler();
+  }, [deleteItemHandler]);
+
+  return (
+    <thead>
+      <tr>
+        <th>{itemName}</th>
+        <th>
+          <button onClick={handleAddItem} disabled={disabledCheck || disabledAddItem}>
+          Добавить {itemName.toLowerCase()}
+          </button>
+        </th>
+        <th>
+          <button onClick={handleDeleteItem} disabled={checkedItemIds.length === 0}>
+          Удалить {itemName.toLowerCase()}
+          </button>
+        </th>
+        <th></th>
+      </tr>
+      <tr>
+        <th>
+          <input
+            type="checkbox"
+            onChange={checkOnChangeHandler}
+            disabled={disabledCheck || disabledAddItem}
+          />{' '}
+        Выделить все
+        </th>
+        {dataForTable.map((el) => (
+          <HeaderItem key={el.id} tableElement={el} />
+        ))}
+      </tr>
+    </thead>
+  );
+};
