f777fbd7284206e2a4eec52e02fc4068f51ecbd2
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-12-31 16:30:01 +0300

    feat(company hook) close logic for edit data companies

---
+++ TheCompany/src/features/company/helpers/UseCompanyHook.ts
@@ -0,0 +1,37 @@
+import {useAppDispatch} from '../../../store';
+import {changeStatusCompany, editCompanyBody, InitialCompanyTypes} from '../reducer/CompanyReducer.ts';
+
+export const UseCompanyHook = (
+  company: InitialCompanyTypes,
+  setIsEditing: (value: boolean) => void,
+  editedName: string,
+  editedAddress: string,
+) => {
+
+  const dispatch = useAppDispatch();
+
+  const onChangeCheckHandler = () => {
+    dispatch(changeStatusCompany(company.id));
+  };
+
+  const onDoubleClickHandler = () => {
+    setIsEditing(true);
+  };
+
+  const onSaveHandler = () => {
+    dispatch(editCompanyBody({companyId: company.id, editedName, editedAddress}));
+    setIsEditing(false);
+  };
+
+  const onKeyDownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onSaveHandler();
+    }
+  };
+
+  return {
+    onDoubleClickHandler,
+    onChangeCheckHandler,
+    onKeyDownHandler,
+  };
+};
