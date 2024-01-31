b814ffd1a2554bc3506f517ba0bc646523d66552
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-01-31 16:29:16 +0300

    feat(WorkersContainer) useMemo optimization

--- TheCompany/src/features/workers/WorkersContainer.tsx
+++ TheCompany/src/features/workers/WorkersContainer.tsx
@@ -1,3 +1,5 @@
+import {memo, useMemo} from 'react';
+
 import {useAppSelector} from '../../store';
 
 import {CommonContainer} from '../../common';
@@ -5,20 +7,21 @@ import {CommonContainer} from '../../common';
 import {WorkersItem} from './item/WorkersItem.tsx';
 import {HeaderWorkers} from './components';
 
-export const WorkersContainer = () => {
-
+export const WorkersContainer = memo(() => {
   const companies = useAppSelector(state => state.company.companies);
   const workers = useAppSelector((state) => state.workers.workers);
 
-  const filteredWorkers = companies
-    .filter((company) => company.isChecked)
-    .flatMap((company) => workers[company.id] || []);
+  const filteredWorkers = useMemo(() => {
+    return companies
+      .filter((company) => company.isChecked)
+      .flatMap((company) => workers[company.id] || []);
+  }, [companies, workers]);
 
   return (
     <CommonContainer
-      headerComponent={<HeaderWorkers companies={companies} />}
-      renderItem={(worker) => <WorkersItem key={worker.id} worker={worker} />}
+      headerComponent={<HeaderWorkers companies={companies}/>}
+      renderItem={(worker) => <WorkersItem key={worker.id} worker={worker}/>}
       items={filteredWorkers}
     />
   );
-};
+});
